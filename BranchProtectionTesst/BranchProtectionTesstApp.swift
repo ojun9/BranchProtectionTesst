e94c8faecf877c65d00d9a67122f65c268f08952
ojun9 <eu.bkw7jdcpa2r@gmail.com>
2023-12-19 12:34:44 +0900

    Initial Commit

---
+++ BranchProtectionTesst/BranchProtectionTesstApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranchProtectionTesstApp.swift
+//  BranchProtectionTesst
+//
+//  Created by jun on 2023/12/19.
+//
+
+import SwiftUI
+
+@main
+struct BranchProtectionTesstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
